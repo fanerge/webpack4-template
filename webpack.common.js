2b8ae4055e500c36b6acda9a2189e8ccb4f43752
fanerge <fanerge@qq.com>
2018-09-04 10:10:40 +0800

    css前缀配置

--- webpack.common.js
+++ webpack.common.js
@@ -23,11 +23,19 @@ module.exports = {
   module: {
     rules: [
       {
+        test:/\.scss$/,
+        use:[
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
         test:/\.css$/,
         use: [
-            MiniCssExtractPlugin.loader,
-            "css-loader",
-            "postcss-loader"
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader"
         ]
       },
       {
